cbb3ba19717ae35eb705e365a4baaa8f49585e12
Ville Henriksson <teettari@hotmail.com>
2016-06-01 12:26:01 +0300

    Release package.

--- TypeScript.Common/Properties/AssemblyInfo.cs
+++ TypeScript.Common/Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@
 [assembly: AssemblyFileVersion("1.5.0.0")]
 
 // This version is used by NuGet:
-[assembly: AssemblyInformationalVersion("1.5.0-prerelease01")]
+[assembly: AssemblyInformationalVersion("1.5.0")]
