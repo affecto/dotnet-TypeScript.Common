f6f4c19fc0cf769f5f18310adfe60e1f374116d0
johannesvaltonen <johannes.valtonen@affecto.com>
2016-06-13 13:44:32 +0300

    Raised version.

--- TypeScript.Common/Properties/AssemblyInfo.cs
+++ TypeScript.Common/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@
 [assembly: AssemblyDescription("TypeScript toolkit including for example comparers, validators and converters.")]
 [assembly: AssemblyCompany("Affecto")]
 
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
 // This version is used by NuGet:
-[assembly: AssemblyInformationalVersion("1.5.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-prerelease01")]
